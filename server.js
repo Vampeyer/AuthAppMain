2b5ce8b32a040cf3bbdc45c34afc061c2352b923
Vampeyer <jthompsonofficial0376@gmail.com>
2025-11-11 20:21:44 +0000

    -

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
+  'http://localhost:5173',
   'http://127.0.0.1',
   'https://techsport.app',
   'https://www.techsport.app',
